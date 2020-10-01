2aaf56b7dd772457b4ab74f3861bce766fde6aab
Daus Carmichael <daviscarmichael@Dauss-MBP.lan>
2020-09-30 20:05:42 -0700

    commit during extreme coding

--- sshell.c
+++ sshell.c
@@ -4,33 +4,43 @@
 
 #define MAX_LINE = 80
 
-
+char** split(const char* str)
+{
+    
+}
 
 int main(void)
 {
     char *args[MAX_LINE/2+1];
     int should_run = 1;
+    const char* arg_string;
+
+    printf("Alarm application starting, ...\n");
+
 
     while(should_run)
     {
         printf("osh>");
         fflush(stdout);
+        gets(arg_string);
+
+        pid_t pid;
+        pid = fork();
+        switch (pid)
+        {
+            case -1:
+                /* Failure */
+                perror("Fork Failed");
+                return -1;
+            default:
+                /* child */
+                wait(NULL);
+                //with argv? or with IO
+                execvp();
+                return 0;
+        }
     }
 
-    printf("Alarm application starting, ...\n");
 
-    pid = fork();
-    switch (pid) {
-        case -1:
-            /* Failure */
-            perror("Fork Failed");
-            return -1;
-        case 0:
-            /* child */
-           wait(NULL);
-           //with argv? or with IO
-           execvp();
-           return 0;
-   }
 
 }
