072ea3651def158ec5523c20d1b8856830556826
Daus Carmichael <daviscarmichael@Dauss-MBP.lan>
2020-09-30 17:16:02 -0700

    read the assignemnt,i dont have big brain

--- sshell.c
+++ sshell.c
@@ -2,14 +2,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define MAX_LINE = 80
 
 
 
-
-int main(int argc, char** argv)
+int main(void)
 {
-    pid_t process_id;
-    process_id = fork();
-    
-    return 0;
+    char *args[MAX_LINE/2+1];
+    int should_run = 1;
+
+    while(should_run)
+    {
+        printf("osh>");
+        fflush(stdout);
+    }
+
+    printf("Alarm application starting, ...\n");
+
+    pid = fork();
+    switch (pid) {
+        case -1:
+            /* Failure */
+            perror("Fork Failed");
+            return -1;
+        case 0:
+            /* child */
+           wait(NULL);
+           //with argv? or with IO
+           execvp();
+           return 0;
+   }
+
 }
