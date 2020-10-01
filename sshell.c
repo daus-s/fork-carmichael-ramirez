ecc81785f220cdcc14ac536fe4ee55b7c400d38d
Daus Carmichael <daviscarmichael@Dauss-MBP.lan>
2020-10-01 00:08:38 -0700

    finished

--- sshell.c
+++ sshell.c
@@ -1,79 +1,105 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define MAX_LINE = 80
+#define MAX_LINE 80
 
-int count_tokens(const char* str)
+
+
+
+int fork_cmd(char** args, bool and)
 {
-
-}
-
-
-char** split(const char* str)
-{
-    int argc = 0;
-    for (int i = 0; i < strlen(str); ++i)
+    pid_t pid;
+    pid = fork();
+    switch (pid)
     {
-        if (str[i]==' ')
-        {
-            if (strlen(token)!=0)
+        case -1:
+            /* Failure */
+            perror("Fork Failed");
+            return -1;
+        case 0:
+            /* child */
+            //with argv? or with IO
+            if (execvp(args[0], args)==-1)
             {
-                argc++;
+                printf("%s: command not found\n", args[0]);
+                return -1;
             }
-        }
-        else
-        {
-            char* token = strcat(token, (char[2]) { (char) str[i], '\0' } );
-        }
+        default:
+            if (!and)
+            {
+                wait(NULL);
+                return 0;
+            }
+
     }
-    //defintely wrong
-    char** args = new char[argc][MAX_LINE];
-    argc = 0;
-    for (int i = 0; i < strlen(str); ++i)
-    {
-        if (str[i]!=' ')
-        {
-            //apend str[i] (char) to token if not space
-            char* token = strcat(token, (char[2]) { (char) str[i], '\0' } );
-        }
-        else
-        {
-            args[argc] = token;
-            argc++;
-        }
-    }
+    return 69;
 }
 
 int main(void)
 {
     char *args[MAX_LINE/2+1];
+    int argc = 0;
     int should_run = 1;
-    const char* arg_string;
+    char* arg_string = malloc(MAX_LINE);
+    bool and = false;
 
-    printf("Alarm application starting, ...\n");
+    printf("application starting, ...\n");
 
 
     while(should_run)
     {
         printf("osh>");
         fflush(stdout);
-        gets(arg_string);
+        fgets(arg_string, MAX_LINE/2+1, stdin);
 
-        pid_t pid;
-        pid = fork();
-        switch (pid)
+        //printf("trying to replace last char\n");
+        //arg_string[strlen(arg_string)-1] = 0;
+        if(arg_string[strlen(arg_string)-1] == '\n')
         {
-            case -1:
-                /* Failure */
-                perror("Fork Failed");
-                return -1;
-            default:
-                /* child */
-                wait(NULL);
-                //with argv? or with IO
-                execvp();
-                return 0;
+            arg_string[strlen(arg_string)-1] = 0;
+        }
+
+        //printf("checking if '&' exists\n");
+        for (size_t i = 0; i < MAX_LINE/2+1; ++i)
+        {
+            if ((and = (arg_string[i]=='&')))
+                break;
+        }
+
+        char* token = strtok(arg_string, " ");
+
+        while(token)
+        {
+            if(*token!='&')
+            {
+                //printf("adding %s to array args\n", token);
+                args[argc]=token;
+            }
+            token = strtok(NULL," ");
+            argc++;
+        }
+        //0 is equal as the difference between the strings (in binary) is 0
+        if (strcmp(args[0], "exit")==0)
+        {
+            printf("exiting...\n");
+            should_run = 0;
+            break;
+        }
+
+
+        fork_cmd(args, and);
+        for (size_t i = 0; i < argc; i++)
+        {
+            strcpy(args[i], "\0");
         }
     }
+
+
+    free(arg_string);
+    return should_run;
 }
--- test.c
+++ test.c
@@ -4,56 +4,58 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 char** split(const char* str)
 {
-    printf("entering split, for string: %s", str);
+    printf("entering split, for string: %s\n", str);
     int argc = 0;
-    char* token = "";
+    //assign maximum size to both each token and total arg length
+    char* token = malloc(80);
+    char args[80][80];
     for (int i = 0; i < strlen(str); ++i)
     {
+        //printf(str[i]==' ' ? "true\n" : "false\n");
         if (str[i]==' ')
         {
+            //printf(strlen(token)!=0 ? "true\n" : "false\n");
             if (strlen(token)!=0)
             {
+
+                strcpy(args[argc], token);
                 argc++;
+                for (int j = 0; j < argc; j++)
+                {
+                      printf("%s,%d/%d\n", args[j],j+1, argc);
+                }
+                //printf("before reset:%s\n", token);
+                strcpy(token, "");
+                //printf("after reset:%s\n", token);
+
+
             }
         }
-        else
+        else //not equal to space char
         {
-            printf("adding char to token");
+            //printf("adding char to token\n");
             token = strcat(token, (char[2]) { (char) str[i], '\0' } );
-            printf("added char to token");
+
+            //add token to args
+            //printf("added char to token, token:%s\n", token);
 
         }
     }
-    //defintely wrong
-    //char** args = char[argc][80];
-    char **args = malloc(80);
-    argc = 0;
-    for (int i = 0; i < strlen(str); ++i)
-    {
-        if (str[i]!=' ')
-        {
-            //apend str[i] (char) to token if not space
-            printf("adding char to token");
-            char* token = strcat(token, (char[2]) { (char) str[i], '\0' } );
-            printf("added char to token");
-        }
-        else
-        {
-            args[argc] = token;
-            argc++;
-        }
-    }
-    return args;
+    char** argv = malloc(80*80);
+    argv = args;
+    return argv;
 }
 
 int main(int argc, char** argv)
 {
-    printf("start program");
-    char** args = split("cp test.c test1.c");
-    for (int j = 0; j < 10; j++ )
+    printf("start program\n");
+    char** args = split(" cp test.c test1.c ");
+    for (int j = 0; j < 3; j++ )
     {
-          printf("%s", args[j]);
+          printf("%s\n", args[j]);
     }
 }
